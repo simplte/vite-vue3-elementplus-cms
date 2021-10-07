c4da4d6007f559233979810fc601656eb81dfe8a
bqc <6575734@qq.com>
2021-10-07 18:35:39 +0800

    feat: 🎸 使用customRef

--- src/views/demo/demoTable.vue
+++ src/views/demo/demoTable.vue
@@ -2,10 +2,12 @@
   <div class="demo-page">{{ testData }}</div>
   <div>asdfasdf</div>
   <ElIcon name="el-icon-success" />
+  <input v-model="keyBoardVal" />
+  {{ keyBoardVal }}
   <div @click="jumpForm()">form</div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, customRef } from 'vue';
 import { ElMessage } from 'element-plus';
 import { useRouter } from 'vue-router';
 export interface TestInter {
@@ -20,15 +22,39 @@ export default defineComponent({
     console.log(123);
     ElMessage.warning('waring');
     const jumpForm = () => {
-      router.push('/demoForm');
+      router.push({ path: '/demo/form' });
     };
     let testData = ref<TestInter>({ name: 'ccc demo' });
+    let keyBoardVal = useDebounce('');
+
     return {
       testData,
       jumpForm,
+      keyBoardVal,
     };
   },
 });
+// 不确定类型所以这里使用泛型
+function useDebounce<T>(value: T, delay = 200) {
+  let timeout;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        // 告诉Vue追踪数据
+        track();
+        return value;
+      },
+      set(newVal: T) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          value = newVal;
+          // 告诉Vue去触发界面更新
+          trigger();
+        }, delay);
+      },
+    };
+  });
+}
 </script>
 
 <style lang="scss" scoped>
