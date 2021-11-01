e6ad67902ddffbfeb037feb880e1ced4ce8d5406
bqc <6575734@qq.com>
2021-11-01 23:10:03 +0800

    feat: 🎸 ts高阶语法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,6 +16,88 @@ export default defineComponent({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     // console.log('import.meta.env', import.meta.env);
+
+    // ts高阶属性使用
+    /**
+     * Record
+     * 定义对象key和value的值
+     */
+    interface PageInfo {
+      title: string;
+    }
+    type Page = 'home' | 'about' | 'contact';
+    const nav: Record<Page, PageInfo> = {
+      home: { title: 'home' },
+      about: { title: 'about' },
+      contact: { title: 'contact' },
+    };
+    console.log(nav);
+    type Name = string;
+    type Members = 'xiaohong' | 'xiaolv' | 'xiaogou';
+    const memberList: Record<Members, Name> = {
+      xiaohong: '123',
+      xiaolv: '123',
+      xiaogou: '123',
+    };
+    console.log(memberList);
+
+    /**
+     * 交叉类型
+     * extend
+     * 将多个类型合并成一个类型
+     */
+    class Person {
+      name: string;
+      constructor(name: string) {
+        this.name = name;
+      }
+      getName() {
+        console.log(this.name);
+      }
+    }
+    interface JobType {
+      isWork: boolean;
+      name: string;
+      startWrok(): boolean;
+    }
+    class Worker implements JobType {
+      isWork: boolean;
+      name: string;
+      constructor(isWork: boolean) {
+        this.isWork = isWork;
+        console.log(isWork);
+        this.name = '123';
+      }
+      startWrok(): boolean {
+        return true;
+      }
+    }
+    // const xiaohai = extend(new Worker(true), new Person('小狗'));
+    // console.log(xiaohai.startWrok());
+
+    /**
+     * 联合类型
+     * 只能访问此联合类型的所有类型里共有的成员
+     */
+    // interface Brid {
+    //   fly(): boolean;
+    //   Jump(): boolean;
+    // }
+
+    // interface Fish {
+    //   topaopao(): boolean;
+    //   swim(): boolean;
+    //   Jump(): boolean;
+    // }
+    // let data: Fish | Brid = {
+    //   Jump(): boolean {
+    //     return true;
+    //   },
+    // };
+    // console.log(data.Jump());
+    // function getSmallPet(): Fish | Brid {}
+    // let pet = getSmallPet();
+    // pet.Jump();
     return {};
   },
 });
@@ -23,11 +105,11 @@ export default defineComponent({
 
 <style lang="scss">
 .home {
-  text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 30vh;
+  text-align: center;
 
   img {
     width: 400px;
